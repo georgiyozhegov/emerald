4729219661d4e9bc63c86a9da2ac762f60290bae
Georgiy Ozhegov <georgiyozhegov.rs@gmail.com>
2025-07-09 14:28:17 +0400

    tokenizer: add module documentation

--- emeraldc/emeraldc-tokenizer/src/lib.rs
+++ emeraldc/emeraldc-tokenizer/src/lib.rs
@@ -1,3 +1,7 @@
+//! Этот модуль ответственен только за разделение входного буфера на отдельные токены. Дальнейший анализ проводится лексером.
+//!
+//! Очень много принципов позаимствовано из [лексера rustc](https://github.com/rust-lang/rust/tree/master/compiler/rustc_lexer).
+
 mod ch_group;
 mod source_buffer;
 mod token;
